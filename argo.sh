73495604c420e442ec08727506baa06f04a3e2bf
RAMBABU <chinnak7416@gmail.com>
2024-08-27 22:57:52 +0530

    Ingress

--- argo.sh
+++ argo.sh
@@ -12,7 +12,7 @@
 #fi
 
 #if [ "$1" == "jobs" ]; then
-  argocd login $((kubectl get ingress -A | grep argocd | awk '{print $4}') --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
+  argocd login $(kubectl get ingress -A | grep argocd | awk '{print $4}') --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
 
   for app in backend frontend ; do
      argocd app create ${app}  --repo https://github.com/devopsb78/expense-helm --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
